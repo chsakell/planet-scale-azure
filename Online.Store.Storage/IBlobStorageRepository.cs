0ffcd2bec28c8fb3a719b597e3294c2791c51d35
chsakell <chsakell@gmail.com>
2017-08-30 20:21:51 +0300

    add storage account project

---
+++ Online.Store.Storage/IBlobStorageRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Threading.Tasks;
+
+namespace Online.Store.Storage
+{
+    public interface IBlobStorageRepository
+    {
+        Task<bool> CreateContainerIfNotExistsAsync(string container);
+        Task SetContainerPermissionsAsync(BlobContainerPermissions permissions);
+        Task UploadFromFileAsync(string blobRef, string filePath);
+        Task<BlobResultSegment> ListBlobsSegmentedAsync();
+        void Init(string container);
+    }
+}
