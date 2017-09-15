bbb2415fd17c13e85d95267167280205eeb4f08a
chsakell <chsakell@gmail.com>
2017-09-15 23:05:55 +0300

    change API URI

--- Online.Store/ClientApp/app/app.constants.ts
+++ Online.Store/ClientApp/app/app.constants.ts
@@ -2,5 +2,5 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Configuration {
-    public Server = 'http://localhost:55190/';
+    public Server = 'http://localhost:57276/';
 }
