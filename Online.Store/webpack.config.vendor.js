f5c6315f88528d754d400ed30d4312f64bf9dad2
chsakell <chsakell@gmail.com>
2017-09-23 16:02:51 +0300

    add font-awesome library

--- Online.Store/webpack.config.vendor.js
+++ Online.Store/webpack.config.vendor.js
@@ -20,6 +20,7 @@ const nonTreeShakableModules = [
     'es6-promise',
     'es6-shim',
     'event-source-polyfill',
+    'font-awesome/css/font-awesome.css',
     'jquery',
 ];
 const allModules = treeShakableModules.concat(nonTreeShakableModules);
