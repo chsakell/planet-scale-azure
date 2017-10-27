cc29a67bbeb1af0bf25acc52f80d10613f728945
chsakell <chsakell@gmail.com>
2017-10-27 22:35:19 +0300

    add comment

--- Online.Store/ClientApp/app/product/store/product.state.ts
+++ Online.Store/ClientApp/app/product/store/product.state.ts
@@ -1,6 +1,7 @@
 import { Product } from './../../models/product';
 import { Cart } from "../../models/cart";
 
+// product state
 export interface ProductState {
     products: Product[],
     selectedProduct?: Product,
