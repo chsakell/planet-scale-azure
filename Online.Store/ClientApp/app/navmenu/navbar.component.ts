6d76abcedfa783bbeaef6894b8a60d8e1cc1ab72
chsakell <chsakell@gmail.com>
2017-11-19 19:28:37 +0200

    view product details after search

--- Online.Store/ClientApp/app/navmenu/navbar.component.ts
+++ Online.Store/ClientApp/app/navmenu/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { CompleterService, CompleterData } from 'ng2-completer';
+import { CompleterService, CompleterData, CompleterItem } from 'ng2-completer';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'nav-bar',
@@ -14,11 +15,17 @@ export class NavBarComponent implements OnInit {
     suggestions: CompleterData;
     searchBy = 'term';//search by paraneter
 
-    constructor(private completerService: CompleterService) {
-        this.suggestions = completerService.remote('/api/search/products?term=', 'description, title, image', 'title')
-            .descriptionField('description')
-            .titleField('title')
-            .imageField('image');
+    constructor(private completerService: CompleterService,
+                private router: Router) {
+        this.suggestions = completerService.remote('/api/search/products?term=', 'title', 'title')
+            .titleField('title');
+    }
+
+    protected viewProduct(item: CompleterItem) {
+        if(item.originalObject) {
+            console.log(item.originalObject);
+            this.router.navigate(['/products/details/' + item.originalObject.id]);
+        }
     }
 
     ngOnInit() { }
