5aa6817a0d1c35a5a277decea804222d6b0ce990
chsakell <chsakell@gmail.com>
2018-01-29 21:03:56 +0200

    app(home): display book images on home page

--- Online.Store/ClientApp/app/home/home.component.ts
+++ Online.Store/ClientApp/app/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.css']
 })
 export class HomeComponent {
 }
